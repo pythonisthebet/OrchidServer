16fa5f615f148c0f7e8a8f79015afed221635be5
User <User@LAB3-3>
2024-10-07 13:53:50 +0300

    added modelsbl for additions to dbcontext without changing original file

---
+++ OrchidServer/ModelsBL/OrchidDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace OrchidServer.Models;
+
+public partial class OrchidDbContext : DbContext
+{
+
+}
+
