0c297b71a1a1944c5477ba162f256341f2cce4ea
noam haner <noamhaner@gmail.com>
2025-04-13 13:19:59 +0300

    added backup from yarden

--- OrchidServer/Controllers/OrchidAPIController.cs
+++ OrchidServer/Controllers/OrchidAPIController.cs
@@ -13,6 +13,7 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Text.Json;
 using System.Security.Cryptography;
+using Microsoft.Data.SqlClient;
 
 namespace OrchidServer.Controllers
 {
@@ -37,6 +38,111 @@ namespace OrchidServer.Controllers
         {
             return Ok("Server Responded Successfully");
         }
+
+        //Helper functions
+        #region Backup / Restore
+        [HttpGet("Backup")]
+        public async Task<IActionResult> Backup()
+        {
+            string path = $"{this.webHostEnvironment.WebRootPath}\\..\\DBScripts\\backup.bak";
+
+            bool success = await BackupDatabaseAsync(path);
+            if (success)
+            {
+                return Ok("Backup was successful");
+            }
+            else
+            {
+                return BadRequest("Backup failed");
+            }
+        }
+
+        [HttpGet("Restore")]
+        public async Task<IActionResult> Restore()
+        {
+            string path = $"{this.webHostEnvironment.WebRootPath}\\..\\DBScripts\\backup.bak";
+
+            bool success = await RestoreDatabaseAsync(path);
+            if (success)
+            {
+                return Ok("Restore was successful");
+            }
+            else
+            {
+                return BadRequest("Restore failed");
+            }
+        }
+        //this function backup the database to a specified path
+        private async Task<bool> BackupDatabaseAsync(string path)
+        {
+            try
+            {
+
+                //Get the connection string
+                string? connectionString = context.Database.GetConnectionString();
+                //Get the database name
+                string databaseName = context.Database.GetDbConnection().Database;
+                //Build the backup command
+                string command = $"BACKUP DATABASE {databaseName} TO DISK = '{path}'";
+                //Create a connection to the database
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    //Open the connection
+                    await connection.OpenAsync();
+                    //Create a command
+                    using (SqlCommand sqlCommand = new SqlCommand(command, connection))
+                    {
+                        //Execute the command
+                        await sqlCommand.ExecuteNonQueryAsync();
+                    }
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+
+        //THis function restore the database from a backup in a certain path
+        private async Task<bool> RestoreDatabaseAsync(string path)
+        {
+            try
+            {
+                //Get the connection string
+                string? connectionString = context.Database.GetConnectionString();
+                //Get the database name
+                string databaseName = context.Database.GetDbConnection().Database;
+                //Build the restore command
+                string command = $@"
+                USE master;
+                ALTER DATABASE {databaseName} SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+                RESTORE DATABASE {databaseName} FROM DISK = '{path}' WITH REPLACE;
+                ALTER DATABASE {databaseName} SET MULTI_USER;";
+
+                //Create a connection to the database
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    //Open the connection
+                    await connection.OpenAsync();
+                    //Create a command
+                    using (SqlCommand sqlCommand = new SqlCommand(command, connection))
+                    {
+                        //Execute the command
+                        await sqlCommand.ExecuteNonQueryAsync();
+                    }
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+        #endregion
+
 
         [HttpPost("login")]
         public IActionResult Login([FromBody] DTO.LoginInfo loginDto)
